e207b5a90702e225a2a8e9b81e7d5940d0b97a96
Георгий Воякин <voyaking@gmail.com>
2022-10-28 17:31:05 +0300

    Update Task2

--- src/main/java/org/example/pr2/Task2.java
+++ src/main/java/org/example/pr2/Task2.java
@@ -1,14 +1,18 @@
 package org.example.pr2;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class Task2 {
     public static void main(String[] args) throws IOException {
@@ -39,9 +43,11 @@ public class Task2 {
         }
 
         // 3
-
+        File copy2 = new File("./copy2.txt");
+        List<String> lines = FileUtils.readLines(file, StandardCharsets.UTF_8);
+        FileUtils.writeLines(copy2, lines);
 
         // 4
-        Files.copy(file.toPath(), Path.of("./copy2.txt"));
+        Files.copy(file.toPath(), Path.of("./copy3.txt"));
     }
 }
