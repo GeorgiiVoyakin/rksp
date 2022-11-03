8beee622b3213f620dec7c1834fab05e469cfea2
GeorgiiVoyakin <voyaking@gmail.com>
2022-11-03 13:43:07 +0300

    Add RxJava

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("commons-io:commons-io:2.11.0")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
